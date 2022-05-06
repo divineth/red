c164d6639c8a853714e55f3cfa828fa3dc5f15bf
divineth <divinethomson@outlook.com>
2022-05-06 01:59:14 +0000

    final update

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+
+
+
+
+        $('.menu-open , .offcanvas-overlay').click(function () {
+
+            $('.offcanvas-area , .offcanvas-overlay').addClass('active');
+
+        });
+        $('.menu-close , .offcanvas-overlay').click(function () {
+
+            $('.offcanvas-area , .offcanvas-overlay').removeClass('active');
+
+        });
+
+
+
+    });
+
+
+}(jQuery));
